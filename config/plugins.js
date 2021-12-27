b1cf0c4f8335999610c944f3e7ce48b2a51c82de
Emily Wangler <emily@at-point.ch>
2021-12-27 12:44:09 +0100

    add cloudinary

---
+++ config/plugins.js
@@ -0,0 +1,15 @@
+module.exports = ({ env }) => ({
+  upload: {
+    provider: 'cloudinary',
+    providerOptions: {
+      cloud_name: env('CLOUDINARY_NAME'),
+      api_key: env('CLOUDINARY_API_KEY'),
+      api_secret: env('CLOUDINARY_API_SECRET'),
+    },
+    actionOptions: {
+      upload: {
+        folder: 'hering'
+      }
+    }
+  },    
+});
