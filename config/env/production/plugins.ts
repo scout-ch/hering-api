46e0afb9ffc8fcabe6b8e28c58f02ea687bb0e18
Mario Zelger <15825982+mario-zelger@users.noreply.github.com>
2025-08-15 18:12:24 +0200

    fix: use explicit s3Options as the current way is deprecated

--- config/env/production/plugins.ts
+++ config/env/production/plugins.ts
@@ -11,17 +11,19 @@ export default ({ env }) => ({
         config: {
             provider: 'aws-s3',
             providerOptions: {
-                credentials: {
-                    accessKeyId: env('OPENSTACK_SWIFT_ACCESS_KEY_ID'),
-                    secretAccessKey: env('OPENSTACK_SWIFT_ACCESS_SECRET'),
-                },
                 baseUrl: `${env('OPENSTACK_SWIFT_ENDPOINT')}/object/v1/${env('OPENSTACK_SWIFT_PUBLIC_AUTH_KEY')}/${env('OPENSTACK_SWIFT_BUCKET')}`,
-                region: env('OPENSTACK_SWIFT_REGION'),
-                endpoint: env('OPENSTACK_SWIFT_ENDPOINT'),
-                forcePathStyle: true,
-                params: {
-                    Bucket: env('OPENSTACK_SWIFT_BUCKET'),
-                },
+                s3Options: {
+                    credentials: {
+                        accessKeyId: env('OPENSTACK_SWIFT_ACCESS_KEY_ID'),
+                        secretAccessKey: env('OPENSTACK_SWIFT_ACCESS_SECRET'),
+                    },
+                    region: env('OPENSTACK_SWIFT_REGION'),
+                    endpoint: env('OPENSTACK_SWIFT_ENDPOINT'),
+                    forcePathStyle: true,
+                    params: {
+                        Bucket: env('OPENSTACK_SWIFT_BUCKET'),
+                    }
+                }
             },
         },
     }
