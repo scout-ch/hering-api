1f8b7cb65fcce6ca66da807b634db9286e38b5c0
Mario Zelger <15825982+mario-zelger@users.noreply.github.com>
2025-10-05 21:31:21 +0200

    fix: trust proxy header fields

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,10 @@
 export default ({ env }) => ({
-  host: env('STRAPI_HOST', '0.0.0.0'),
-  port: env.int('STRAPI_PORT', 1337),
-  app: {
-    keys: env.array('STRAPI_APP_KEYS'),
-  },
+    host: env('STRAPI_HOST', '0.0.0.0'),
+    port: env.int('STRAPI_PORT', 1337),
+    app: {
+        keys: env.array('STRAPI_APP_KEYS'),
+    },
+    proxy: {
+        koa: true
+    }
 });
