8c5488dffb062cec9cbc7dab51e719a3db677108
Mario Zelger <15825982+mario-zelger@users.noreply.github.com>
2025-10-30 00:47:38 +0100

    fix: added missing Strapi url config

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,7 @@
 export default ({ env }) => ({
     host: env('STRAPI_HOST', '0.0.0.0'),
     port: env.int('STRAPI_PORT', 1337),
+    url: env('STRAPI_URL'),
     app: {
         keys: env.array('STRAPI_APP_KEYS'),
     },
